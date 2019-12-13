296fec1fab08f31b9f0293a277a52efb8d0660dc
Pavel Zak <izakpavel@gmail.com>
2019-12-13 08:29:25 +0100

    created empty project

---
+++ HappyShapy/HappyShapy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HappyShapy
+//
+//  Created by myf on 13/12/2019.
+//  Copyright © 2019 nerdyak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
